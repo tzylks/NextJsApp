97d625a8fe6374ffd97dd9a48d2d29217431c099
TrevorZylks <trevorzylks@gmail.com>
2023-05-04 20:59:54 -0700

    Oauth finally implemented

--- example-app/src/components/NavBar.tsx
+++ example-app/src/components/NavBar.tsx
@@ -1,13 +1,14 @@
+import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import Link from 'next/link';
-import React, { FC } from 'react';
-import { buttonVariants } from '@/ui/Button';
-import SignInButton from '@/components/SignInButton';
+import { ThemeToggle } from './ThemeToggle';
+import { buttonVariants } from './ui/Button';
 import SignOutButton from '@/components/SignOutButton';
-import { ThemeToggle } from '@/components/ThemeToggle';
+import SignInButton from '@/components/SignInButton';
 
 const NavBar = async () => {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
+
     return (
         <div className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between'>
             <div className='container max-w-7xl mx-auto w-full flex justify-between items-center'>
